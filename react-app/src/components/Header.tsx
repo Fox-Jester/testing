508e0042061d1a1d8e466ed30326b7068e1f8af2
William Marshall <willmarshall2424@gmail.com>
2025-08-26 18:38:44 -0400

    feat: add Header component

---
+++ react-app/src/components/Header.tsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+
+    return(
+        <header>
+            <p>
+                <span>NC</span>
+                Expolorer
+            </p>
+
+            <p>searrch icon</p>
+
+            <p>locations</p>
+            <p>about</p>
+        </header>
+    )
+}
+
+export default Header
