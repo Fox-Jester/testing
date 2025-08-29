f06b71560ca4ff8ba6ec460cdc71af3576aebc78
William Marshall <willmarshall2424@gmail.com>
2025-08-29 10:03:40 -0400

    feat: add PageTitle

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 
-import Header from "./components/Header"
+import Header from "./components/Header.tsx"
+import PageTitle from "./components/PageTitle.tsx"
 
 function App(){
 
@@ -7,6 +8,7 @@ function App(){
 return (
     <>
     <Header></Header>
+    <PageTitle></PageTitle>
     </>
 )
         
---
+++ react-app/src/components/PageTitle.tsx
@@ -0,0 +1,10 @@
+
+
+export default function PageTitle(){
+    return(
+        <div className="title">
+            <h1>Popular NorthCarolina Landmarks</h1>
+            <p>Stunning sites to see in the state of NorthCarolina!</p>
+        </div>
+    )
+}
